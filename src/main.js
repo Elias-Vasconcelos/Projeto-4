296424ff05baca09e9bb267114253e634fc5dd9a
Elias-Vasconcelos <146135801+Elias-Vasconcelos@users.noreply.github.com>
2024-08-12 22:59:13 -0300

    Ajustes Responsivos

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(){
     const NavButtons = document.querySelectorAll('[data-tab-button]');
-    
+    const faqQuestion = document.querySelectorAll('[data-faq-item]');
+
+    const HeroSection = document.querySelector('.hero')
+    const HeroHeight = HeroSection.clientHeight;
+    window.addEventListener('scroll', function(){
+        const screanPosition = window.scrollY;
+        if(screanPosition < HeroHeight ){
+            ShowHeader();
+        }else{
+            HidenHeader();
+        }
+    } )
+
     for (i=0; i < NavButtons.length; i++) {
         NavButtons[i].addEventListener('click', function(Focus){
             const TargetButton = Focus.target.dataset.tabButton;
@@ -8,11 +20,29 @@ document.addEventListener('DOMContentLoaded', function(){
             TabRemove ();
             TargetTab.classList.add('whatch-content--active');
             BorderRemove();
-            Focus.target.classList.add('whatch-list__button--active')
-
+            Focus.target.classList.add('whatch-list__button--active');
         } )
     }
+    for (i=0; i < faqQuestion.length; i++) {
+        faqQuestion[i].addEventListener('click',ToggleFaq )
+    }
+
 })
+
+function ShowHeader(){
+    const header = document.querySelector('.header')
+    header.classList.add('header--on-site')
+}
+function HidenHeader(){
+    const header = document.querySelector('.header')
+    header.classList.remove('header--on-site')
+}
+
+function ToggleFaq(elemento){
+    const ClassFaq = 'faq-lis__item--is-active';
+    const father = elemento.target.parentNode;
+    father.classList.toggle(ClassFaq);
+}
 
 function BorderRemove (){
     const Border = document.querySelectorAll('[data-tab-button]');
